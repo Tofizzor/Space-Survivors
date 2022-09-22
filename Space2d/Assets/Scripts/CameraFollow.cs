27bb397bf45556cccd3b90bcc0d68b80f8920dbd
Justas <46451808+Tofizzor@users.noreply.github.com>
2022-09-22 23:38:42 +0100

    Added movement

---
+++ Space2d/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 offset;
+
+    private void FixedUpdate()
+    {
+        transform.position = target.position+offset;
+    }
+}
