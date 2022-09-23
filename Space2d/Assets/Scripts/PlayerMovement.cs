335af85e238598b97270d9526759c71d6464c50e
eddnbacon <edbox52@gmail.com>
2022-09-23 13:58:23 +0100

    boost animation

--- Space2d/Assets/Scripts/PlayerMovement.cs
+++ Space2d/Assets/Scripts/PlayerMovement.cs
@@ -7,8 +7,10 @@ using UnityEngine.EventSystems;
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D ship;
-    public float moveSpeed = 1f;
+    public float moveSpeed = 100f;
+    public float maxVelocity = 10f;
     public float rotationSpeed = 90f;
+    public Animator animator;
     private float moveHorizontal;
     private float moveVertical;
     private Vector2 movementDirection;
@@ -23,15 +25,19 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        animator.SetBool("SpacePressed", false);
         moveHorizontal = Input.GetAxisRaw("Horizontal");
         moveVertical = Input.GetAxisRaw("Vertical");
 
         movementDirection = new Vector2(moveHorizontal, moveVertical);
         inputMagnitude = Mathf.Clamp01(movementDirection.magnitude);
         movementDirection.Normalize();
+        ship.velocity = Vector2.ClampMagnitude(ship.velocity, maxVelocity);
         Debug.Log(Input.GetKey(KeyCode.Space));
         if (Input.GetKey("space"))
         {
+            animator.SetBool("SpacePressed", true);
+
             if (Input.GetKey("up") || Input.GetKey("down") || Input.GetKey("left") || Input.GetKey("right"))
             {
                 ship.AddForce(movementDirection * moveSpeed * inputMagnitude * Time.deltaTime);
@@ -47,7 +53,6 @@ public class PlayerMovement : MonoBehaviour
 
         if (movementDirection != Vector2.zero)
         {
-            
             transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
 
         }
